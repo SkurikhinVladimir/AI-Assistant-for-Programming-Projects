b7bd392ecefb4d2dbac9b035c712b0b9dc740563
v.skurikhin <v.skurikhin@innopolis.ru>
2024-09-12 16:08:55 +0700

    Added build folder

---
+++ streamlit_tree_select/frontend/build/precache-manifest.56f8a850ffff7e23f3856a86a6a232db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c50c69a5897f77043f3aaf45bee802",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a6a78a3b77889c1c1af",
+    "url": "./static/js/2.15ada35c.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.15ada35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aada34287bd66701f129",
+    "url": "./static/js/main.74854d48.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
---
+++ streamlit_tree_select/frontend/build/precache-manifest.8d300b96e6193fdeb27135eacb72a2ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2661d563119c7c6cc6c455a592cf1d2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a6a78a3b77889c1c1af",
+    "url": "./static/js/2.15ada35c.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.15ada35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b0f6911024b7d1e729",
+    "url": "./static/js/main.28667969.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
